5d6cda9e3eb57e853b226d8e000ec881a6ec0548
Gesim, Christian Dave C. <129653013+gesim-dave123@users.noreply.github.com>
2025-10-23 10:13:27 +0800

    Delete Midterm Exam directory

--- Midterm Exam/helpers/math_utils.py
+++
@@ -1,7 +0,0 @@
-def area(length, width):
-    return length * width
-
-def area_improved(length: float, width: float) -> float:
-    if not (isinstance(length, (int, float)) and isinstance(width, (int, float))):
-        return "Length and width must be numbers"
-    return length * width
--- Midterm Exam/helpers/string_utils.py
+++
@@ -1,8 +0,0 @@
-def shout(s) :
-    return s.upper()
-
-def shout_improved(s: str) -> str:
-    """Return the string in uppercase."""
-    if not isinstance(s, str):
-        return "Input must be a string"
-    return s.upper()
